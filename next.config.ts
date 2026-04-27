581d4d94834e433bb47efc3d552ac09f5c01553a
hugo <hugo@hugo-golliet.dev>
2026-04-27 15:10:39 +0200

    chore: remove remote pattern of image, useless now with minio

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type {NextConfig} from 'next';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3001';
-
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [new URL(API_URL + '/images/**')],
-  },
   output: 'standalone',
 };
 
