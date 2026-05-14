ae36dfaa29b39badab7ad549170b03912e123aa8
hugo <hugo@hugo-golliet.dev>
2026-05-14 14:34:35 +0200

    fix(minio): handle exceptions & use ssl option

--- lib/minio.ts
+++ lib/minio.ts
@@ -7,13 +7,16 @@ const bucket = process.env.NEXT_PUBLIC_MINIO_BUCKET;
 const minio = new Client({
   endPoint: process.env.NEXT_PUBLIC_MINIO_ENDPOINT,
   port: Number(process.env.NEXT_PUBLIC_MINIO_PORT),
-  useSSL: true,
-  accessKey: process.env.NEXT_PUBLIC_MINIO_ROOT_USER,
-  secretKey: process.env.NEXT_PUBLIC_MINIO_ROOT_PASSWORD,
+  useSSL: process.env.NEXT_PUBLIC_MINIO_USESSL === 'true',
+  accessKey: process.env.MINIO_ROOT_USER,
+  secretKey: process.env.MINIO_ROOT_PASSWORD,
 });
 
 export const getImageUrl = async (name: string) => {
-  const url = await minio.presignedGetObject(bucket, name, 3600);
-
-  return url;
+  try {
+    const url = await minio.presignedGetObject(bucket, name, 3600);
+    return url;
+  } catch {
+    return null;
+  }
 };
