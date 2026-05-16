d388d41e052100eba9020f3adebcc500b6db52c4
hugo <hugo@hugo-golliet.dev>
2026-05-16 11:11:33 +0200

    feat: go home button on error

--- components/shared/error.tsx
+++ components/shared/error.tsx
@@ -2,13 +2,23 @@
 
 import {AlertCircleIcon} from 'lucide-react';
 import {Alert, AlertDescription, AlertTitle} from '../ui/alert';
+import {useRouter} from 'next/navigation';
+import {Button} from '../ui/button';
 
 export const ErrorComponent = ({error}: {error: string | null}) => {
+  const {push} = useRouter();
+
   return (
-    <Alert variant="destructive" className="w-fit">
-      <AlertCircleIcon />
-      <AlertTitle>Error !</AlertTitle>
-      <AlertDescription>{error ?? 'There was an error but it is unknown'}</AlertDescription>
-    </Alert>
+    <div className="space-y-2">
+      <Alert variant="destructive" className="w-fit">
+        <AlertCircleIcon />
+        <AlertTitle>Error !</AlertTitle>
+        <AlertDescription>{error ?? 'There was an error but it is unknown'}</AlertDescription>
+      </Alert>
+
+      <Button className="w-full" onClick={() => push('/')}>
+        Go Home
+      </Button>
+    </div>
   );
 };
